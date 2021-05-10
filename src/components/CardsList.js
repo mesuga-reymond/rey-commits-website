ab1029605b15ba3b244af39b6dc62adb09577e15
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-11 00:09:57 +0800

    CardList that displays the cards has been modified

---
+++ src/components/CardsList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Container, Row} from "react-bootstrap"
+import IndividualCard from './IndividualCard'
+import {studyData} from "../studyData"
+
+function CardsList() {
+    
+    return (
+        <Container className="cardlist-container">
+            <Row md={3} >
+            {studyData.map(study => (<IndividualCard title={study.title} info={study.info} img={study.img}/>))}
+            </Row>            
+        </Container>
+    )
+}
+
+export default CardsList
