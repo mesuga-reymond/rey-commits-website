fb0a78c84436f107c9b055677fb91b1321bcafa6
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-11 00:34:39 +0800

    CardList component has been modified

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -7,7 +7,7 @@ function CardsList() {
     
     return (
         <Container className="cardlist-container">
-            <Row md={3} >
+            <Row md={3}>
             {studyData.map(study => (<IndividualCard title={study.title} info={study.info} img={study.img}/>))}
             </Row>            
         </Container>
