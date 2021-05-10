298660673e3c5c05e04870b6a67519ad2b0b9c64
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-11 00:11:13 +0800

    dummy data has been added

---
+++ src/studyData.js
@@ -0,0 +1,44 @@
+export const studyData = [
+    {
+      id: 1,
+      title: "Polompon, Leyte",
+      img: "https://dystonia-foundation.org/wp-content/uploads/2019/03/colorful-brain-network-375w.jpg",
+      author: "Kalanggaman Island Tour",
+      info:
+        "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature."
+    },
+    {
+      id: 2,
+      title: "Polompon, Leyte",
+      img: "https://dystonia-foundation.org/wp-content/uploads/2019/03/colorful-brain-network-375w.jpg",
+      author: "Kalanggaman Island Tour",
+      info:
+        "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature."
+    },
+    {
+      id: 3,
+      title: "Polompon, Leyte",
+      img: "https://dystonia-foundation.org/wp-content/uploads/2019/03/colorful-brain-network-375w.jpg",
+      author: "Kalanggaman Island Tour",
+      info:
+        "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature."
+    },
+    {
+      id: 4,
+      title: "Polompon, Leyte",
+      img: "https://dystonia-foundation.org/wp-content/uploads/2019/03/colorful-brain-network-375w.jpg",
+      author: "Kalanggaman Island Tour",
+      info:
+        "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature."
+    },
+    {
+      id: 5,
+      title: "Polompon, Leyte",
+      img: "https://dystonia-foundation.org/wp-content/uploads/2019/03/colorful-brain-network-375w.jpg",
+      author: "Kalanggaman Island Tour",
+      info:
+        "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature."
+    },
+    
+  ];
+  
