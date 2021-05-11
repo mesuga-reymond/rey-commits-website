1059c780cb8d16cee058973070d0b92b838085f5
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-11 12:42:25 +0800

    links have been fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,31 +3,37 @@ import React from 'react'
 import {Jumbotron, Container, Button, Col} from "react-bootstrap"
 function Hero() {
     function openTabGithub() {
-            window.open('https://github.com/Rey-commits');
-        }
-        function openTabLinkedIn() {
-            window.open('https://github.com/Rey-commits');
-        }
-        function openTabFacebook() {
-            window.open('https://github.com/Rey-commits');
-        }
-    return ( 
+        window.open('https://github.com/Rey-commits');
+    }
+    function openTabLinkedIn() {
+        window.open('https://www.linkedin.com/in/reymondmesuga-421/');
+    }
+    function openTabFacebook() {
+        window.open('https://www.facebook.com/reymond.mesugah/');
+    }
+    return (
         <div>
             <Jumbotron fluid className="hero">
                 <Container className="hero-container">
-                    <h1>Data Analysis & Artificial Intelligence Projects</h1>
+                    <h1>Data Analysis & Machine Learning Projects</h1>
                     <h4>by Reymond Mesuga</h4>
                     <p>
                         This is a modified jumbotron that occupies the entire horizontal space of its
                         parent.
                     </p>
-                    <Col>
-                        <a className="individual-button" onClick={openTabGithub}><Button variant="outline-light">GitHub</Button></a>
-                        <a className="individual-button" onClick={openTabLinkedIn}><Button variant="outline-light">LinkedIn</Button></a>
-                        <a className="individual-button" onClick={openTabFacebook}><Button variant="outline-light">Facebook</Button></a>                  
+                    <Col className="hero-buttons-column">
+                        <a onClick={openTabGithub}>
+                            <Button className="individual-button" variant="outline-light"><i class="fab fa-github-square social-icon"></i> GitHub</Button>
+                        </a>
+                        <a onClick={openTabLinkedIn}>
+                            <Button className="individual-button" variant="outline-light"><i class="fab fa-linkedin social-icon"></i> LinkedIn</Button>
+                        </a>
+                        <a onClick={openTabFacebook}>
+                            <Button className="individual-button" variant="outline-light"><i class="fab fa-facebook-square social-icon"></i> Facebook</Button>
+                        </a>
                     </Col>
                 </Container>
-            </Jumbotron> 
+            </Jumbotron>
         </div>
     )
 }
