8bd3cb6a68c68d94dc21fc86ecb589784e83c577
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-12 09:46:33 +0800

    content table has been imported

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Navigation from "./components/Navigation"
 import Hero from './components/Hero';
 import CardsList from './components/CardsList';
 import Footer from './components/Footer';
+import ContentTable from './components/ContentTable';
 
 function App() {
     return (
         <div className="App">
             <Navigation />
             <Hero />
+            <ContentTable />
             <CardsList />
             <Footer />
         </div>
