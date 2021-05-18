6c91b244fd152d3b8af1801ffc0f68c667cc21de
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-19 00:07:12 +0800

    RandomQuotes has been removed

--- src/components/ContentTable.js
+++ src/components/ContentTable.js
@@ -1,48 +1,13 @@
 import React from 'react'
-import {Accordion, Button, Card} from 'react-bootstrap'
+import {Accordion, Card} from 'react-bootstrap'
 import {studyData} from "../studyData"
 
 function ContentTable() {
-    const [quotes,
-        setQuotes] = React.useState([]);
-    const [randomQuote,
-        setRandomQuote] = React.useState("")
-
-    React.useEffect(() => {
-        async function fetchData() {
-            const response = await fetch("https://type.fit/api/quotes")
-            const data = await response.json();
-
-            setQuotes(data);
-            let randIndex = Math.floor(Math.random() * data.length);
-            setRandomQuote(data[randIndex])
-        }
-        fetchData();
-    }, [])
-
-    const getNewQuote = () => {
-        let randIndex = Math.floor(Math.random() * quotes.length);
-        setRandomQuote(quotes[randIndex])
-    }
+    
     return (
 
         <div>
-                <div className="quote-card">
-                    <Card>
-                        <Card.Header className="quote-header">Quotes</Card.Header>
-                        <Card.Body>
-                            <blockquote className="blockquote mb-0">
-                            <p className="card-text">&quot;{randomQuote.text}&quot;</p>
-                                <footer className="blockquote-footer">
-                                <h5 className="card-title">- {randomQuote.author  || "No author"}</h5>
-                                </footer>
-                            </blockquote>
-                            <Button onClick={getNewQuote} variant="outline-secondary">New Quote</Button>
-                        {/* <a href="" className="btn btn-warning" ></a>
-                        <a href=""></a> */}
-                        </Card.Body>
-                    </Card>
-                </div>
+                
             <Accordion className="content-table" defaultActiveKey="0">
                 
                 {/* <p className="instructions">Consider checking the list of content below to ease finding a project in your interest. Also, be patient while loading source code on GiHub because some of the files are large enough to load a few minutes.</p> */}
