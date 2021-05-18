41d0cedccc3ac92d0e0872ed17f8490542526d06
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-18 23:05:45 +0800

    data content have been modified

--- src/studyData.js
+++ src/studyData.js
@@ -1,17 +1,17 @@
 export const studyData = [
     {
       id: 1,
-      title: "Pneumonia Image Classification Using CNN",
+      title: "Pneumonia Image Classification Using CNN with ResNet9 Architecture",
       img: "https://balance.hcr-manorcare.com/media/6027/pneumoniaxray.jpg?width=799&height=499&mode=crop",
-      author: "Reymond Mesuga",
+      author: "Reymond R. Mesuga",
       info:
-        "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature."
+        "This is a pneumonia classifier model in CNN with ResNet9 architecture using PyTorch."
     },
     {
       id: 2,
       title: "Hotel Cancelation Prediction Using Simple Deep Learning Model",
       img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/3a/4e/2c/exterior.jpg?w=900&h=-1&s=1",
-      author: "Reymond Mesuga",
+      author: "Reymond R. Mesuga",
       info:
         "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature."
     },
@@ -25,19 +25,19 @@ export const studyData = [
     },
     {
       id: 4,
-      title: "Covid-19 Data Analysis Using Python",
+      title: "Covid-19 Exploratory Data Analysis Using Python",
       img: "https://news.stv.tv/wp-content/uploads/2020/05/GettyImages-1213578032.jpg",
-      author: "Reymond Mesuga",
+      author: "Reymond R. Mesuga",
       info:
-        "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature."
+        "This project is divided in two parts. First is about ranking analysis of countries and the second is about relationship between health indicators and total Covid-19 cases."
     },
     {
       id: 5,
-      title: "[Ongoing] Comparison of Different Data Augmentation Techniques to Improve the Accuracy of CNN to Classify Glitches that Interupts the Gravitational Wave Detection",
+      title: "[Ongoing] Comparative Analysis on the Accuracy of Different CNN Architectures to Classify Glitches that Interupts the Gravitational Wave Detection in LIGO",
       img: "https://assets.weforum.org/article/image/large_eTaYzNWUEhMHG0CTdPe1y3WoahZDp-CUf40WxUh2EKk.jpg",
       author: "Reymond Mesuga and Brian James Bayanay",
       info:
-        "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature."
+        "This academic reseach aims to compare different state-of-the-art CNN architectures to classify glitches detected by LIGO. The outcomes of this research may give insights to improve current CNN models for future benefits."
     },
     {
       id: 6,
@@ -45,7 +45,7 @@ export const studyData = [
       img: "https://els-jbs-prod-cdn.jbs.elsevierhealth.com/cms/attachment/2000890145/2002976543/gr1.jpg",
       author: "Reymond Mesuga, Cloyd Raymond Pernes and Luther Villacruz",
       info:
-        "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature."
+        "This academic research aims to investigate the effects of image quality on the accuracy of CNN to predict the possible existence of Covid-19 virus on x-ray images. This will be observed by manipulating images at different image quality distortions. The outcomes of this study may give insights to improve both medical imaging technologies and deep learning algorithms."
     },
     
   ];
