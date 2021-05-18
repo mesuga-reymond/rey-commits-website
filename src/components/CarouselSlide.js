0c6e2b0eb3d1fe3d5d08285bfc43ae00105053f1
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-18 22:11:17 +0800

    CorouselSlide content has been modified

--- src/components/CarouselSlide.js
+++ src/components/CarouselSlide.js
@@ -7,31 +7,32 @@ function CarouselSlide() {
             <Carousel fade>
                 <Carousel.Item interval={5000}>
                     <img
-                        className="carousel-image d-block w-100"
+                        className="carousel-image"
                         src="https://threatwarrior.com/wp-content/uploads/2019/01/tw-press-neural-1024x288.png"
                         alt="First slide"/>
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3>Data Analysis</h3>
+                        <p>I use python as my main language when it comes to data analysis. 
+                        Datasets involve in my studies ranging from health, socio-economic and industrial datasets</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item interval={5000}>
                     <img
-                        className="d-block w-100"
+                        className="carousel-image"
                         src="https://threatwarrior.com/wp-content/uploads/2019/01/tw-press-neural-1024x288.png"
                         alt="Second slide"/>
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h3>Machine and Deep Learning</h3>
+                        <p>I use PyTorch and Tensorflow on most of my projects that mainly focuses on prediction and classification</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item interval={5000}>
                     <img
-                        className="d-block w-100"
+                        className="carousel-image"
                         src="https://threatwarrior.com/wp-content/uploads/2019/01/tw-press-neural-1024x288.png"
                         alt="Third slide"/>
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
+                        <h3>Wep App Development</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
