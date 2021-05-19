d49995a024d469ca27bde290d13301976aec9944
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-20 00:37:27 +0800

    react router have been implemented

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from "./components/Navigation"
@@ -6,18 +8,29 @@ import CardsList from './components/CardsList';
 import Footer from './components/Footer';
 import ContentTable from './components/ContentTable';
 import CarouselSlide from './components/CarouselSlide';
-import RandomQuotes from './components/RandomQuotes';
+import About from './components/About';
+import MainBody from './MainScreens/MainBody';
 
 function App() {
     return (
         <div className="App">
+        <Router>
             <Navigation />
             <Hero />
-            <RandomQuotes />
+            
+                <Switch>
+                <Route exact path="/" component={MainBody} />
+                <Route path='/about' component={About} />
+                </Switch>
+            <CarouselSlide />
+            <Footer />
+            {/* <RandomQuotes />
             <ContentTable />
             <CardsList />
             <CarouselSlide />
-            <Footer />
+            <Footer /> */}
+        </Router>
+            
         </div>
     );
 }
