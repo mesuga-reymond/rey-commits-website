52344d96f9e9c1da4f271d14e8de467caad45d40
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-20 00:42:51 +0800

    data content has been modified

--- src/studyData.js
+++ src/studyData.js
@@ -4,46 +4,59 @@ export const studyData = [
       title: "Pneumonia Image Classification Using CNN with ResNet9 Architecture",
       img: "https://balance.hcr-manorcare.com/media/6027/pneumoniaxray.jpg?width=799&height=499&mode=crop",
       author: "Reymond R. Mesuga",
+      type: "Personal Project",
+      topics: "Deep Learning, CNN, ResNet9, Pneumonia",
       info:
         "This is a pneumonia classifier model in CNN with ResNet9 architecture using PyTorch."
     },
     {
       id: 2,
-      title: "Hotel Cancelation Prediction Using Simple Deep Learning Model",
+      title: "Hotel Cancelation Prediction Using Simple Deep Learning Model in Keras",
       img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/3a/4e/2c/exterior.jpg?w=900&h=-1&s=1",
       author: "Reymond R. Mesuga",
+      type: "Personal Project",
+      topics: "Deep Learning, Prediction, Keras Model",
       info:
-        "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature."
+        "This project is a simple deep learning model aim to predict hotel cancelations. The techniques used in this project can be applied as well to make predictions using different datasets."
     },
     {
       id: 3,
       title: "Covid-19 Chest X-Ray Image Classification Using Different CNN Architectures",
       img: "https://www.bronkhorst.com/getmedia/ee6c6ce7-8e23-47dd-8ef4-e7e00c23850b/BHT-COVID-19.png",
       author: "Reymond R. Mesuga",
+      type: "Personal Project",
+      topics: "Deep Learning, Classification, Transfer Learning, CNN, Covid-19",
       info:
         "The goal of this project is to compare the accuracy of different state of the art models to predict the possible existence of Covid-19 using chest x-ray images."
     },
     {
       id: 4,
-      title: "[Ongoing] Comparative Analysis on the Accuracy of Different CNN Architectures to Classify Glitches that Interupts the Gravitational Wave Detection in LIGO",
+      title: "[Ongoing] Comparative Analysis on the Accuracy of Different Convolutional Neural Network Architectures to Classify Glitches that Interupts the Gravitational Wave Detection in LIGO",
       img: "https://assets.weforum.org/article/image/large_eTaYzNWUEhMHG0CTdPe1y3WoahZDp-CUf40WxUh2EKk.jpg",
       author: "Reymond Mesuga and Brian James Bayanay",
+      type: "Academic Research",
+      topics: "Deep Learning, CNN, Transfer Learning, Gravitational Waves, Modern Physics, LIGO",
       info:
-        "This academic reseach aims to compare different state-of-the-art CNN architectures to classify glitches detected by LIGO. The outcomes of this research may give insights to improve current CNN models for future benefits."
+        "This academic research aims to compare different state-of-the-art CNN architectures to classify glitches detected by LIGO. The outcomes of this research may give insights to improve current CNN models for future benefits of LIGO."
     },
     {
       id: 5,
       title: "Covid-19 Exploratory Data Analysis Using Python",
       img: "https://news.stv.tv/wp-content/uploads/2020/05/GettyImages-1213578032.jpg",
+      modalImage:"",
       author: "Reymond R. Mesuga",
+      type: "Personal Project",
+      topics: "Data Analysis, Covid-19, Python",
       info:
-        "This project is divided in two parts. First is about ranking analysis of countries and the second is about relationship between health indicators and total Covid-19 cases."
+        "This project is divided into two parts. First is about ranking analysis of countries in terms of total cases. Second is about relationship between health indicators and total Covid-19 cases."
     },
     {
       id: 6,
-      title: "[Ongoing] Investigation on the Effects of Image Quality Distortions on the Accuracy of CNN to Predict the Possible Existence of Covid-19 Using Chest X-ray Images",
+      title: "[Ongoing] Investigation on the Effects of Image Quality Distortions on the Accuracy of Convolutional Neural Networks to Predict the Possible Existence of Covid-19 Using Chest X-ray Images",
       img: "https://els-jbs-prod-cdn.jbs.elsevierhealth.com/cms/attachment/2000890145/2002976543/gr1.jpg",
       author: "Reymond Mesuga, Cloyd Raymond Pernes and Luther Villacruz",
+      type: "Academic Research",
+      topics: "Deep Learning, Classification, CNN, Transfer Learning, Covid-19, Image Quality, Image Manipulation, Algorithms",
       info:
         "This academic research aims to investigate the effects of image quality on the accuracy of CNN to predict the possible existence of Covid-19 virus on x-ray images. This will be observed by manipulating images at different image quality distortions. The outcomes of this study may give insights to improve both medical imaging technologies and deep learning algorithms."
     },
