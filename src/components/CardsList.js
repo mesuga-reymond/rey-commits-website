3584db062f89182a36a67d2b32980032b91eebe4
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-20 00:38:01 +0800

    breadcrumb has been added

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import {Container, Row} from "react-bootstrap"
+import {Breadcrumb, Container, Row} from "react-bootstrap"
+import {LinkContainer} from 'react-router-bootstrap'
+
 import IndividualCard from './IndividualCard'
 import {studyData} from "../studyData"
 
@@ -7,8 +9,27 @@ function CardsList() {
     
     return (
         <Container className="cardlist-container">
+            <Container className="breadcrumb-container">
+                <Breadcrumb>
+
+                    <LinkContainer to="/">
+                        <Breadcrumb.Item active>
+                            Projects
+                        </Breadcrumb.Item>
+                    </LinkContainer>
+                </Breadcrumb>
+            </Container>
             <Row className="cardlist-row" md={2} sm={1} lg={3} >
-            {studyData.map(study => (<IndividualCard title={study.title} author={study.author} info={study.info} img={study.img}/>))}
+            {studyData.map(study => (<IndividualCard 
+            key={study.id} 
+            title={study.title} 
+            author={study.author} 
+            info={study.info} 
+            img={study.img} 
+            type={study.type}
+            modalImage={study.modalImage}
+            topics={study.topics}   
+            />))}
             </Row>            
         </Container>
     )
