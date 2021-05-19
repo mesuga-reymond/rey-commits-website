a9ae0913163cae1333565a625de9719fa4bb4bd0
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-20 00:39:29 +0800

    modal has been added

--- src/components/IndividualCard.js
+++ src/components/IndividualCard.js
@@ -1,7 +1,12 @@
-import React from 'react'
-import {Card, Button} from "react-bootstrap"
+import React, {useState} from 'react'
+import {Card, Button, Modal, Jumbotron, Container} from "react-bootstrap"
 
-function IndividualCard({title,info,img,author}) {
+function IndividualCard({title, info, img, author, type, modalImage, topics}) {
+    const [lgShow,
+        setLgShow] = useState(false);
+    // const BackgroundImageModal = {
+    //     backgroundImage: img
+    // }
     return (
         <div className="individual-card">
             <Card md={3} style={{
@@ -10,11 +15,37 @@ function IndividualCard({title,info,img,author}) {
                 <Card.Img variant="top" src={img}/>
                 <Card.Body>
                     <Card.Title>{title}</Card.Title>
-                    <p><i>by {author}</i></p>
+                    <p>
+                        <i>by {author}</i>
+                    </p>
                     <Card.Text>
                         {info}
                     </Card.Text>
-                    <Button variant="dark">GitHub</Button>
+                    <Button onClick={() => setLgShow(true)} variant="dark">Read more</Button>
+                    <Modal
+                        size="lg"
+                        show={lgShow}
+                        onHide={() => setLgShow(false)}
+                        aria-labelledby="example-modal-sizes-title-lg">
+                        <Modal.Header closeButton>
+                            <Modal.Title id="example-modal-sizes-title-lg">
+                                {type}
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Jumbotron fluid>
+                                <Container style={{backgroundImage: `url(${modalImage})`}}>
+                                    <div className="modal-jumbotron">
+                                    <h2>{title}</h2>
+                                    <div><i>by {author}</i></div>
+                                    </div>
+                                    <hr></hr>
+                                </Container>
+                            </Jumbotron>
+                            <p>{info}</p>
+                        </Modal.Body>
+                        <div className="modal-footer">Topics: {topics}</div>
+                    </Modal>
                 </Card.Body>
             </Card>
         </div>
