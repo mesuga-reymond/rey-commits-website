bae23aaff66aca2fb253b50a6c9b1d2128bb89f4
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-21 11:10:58 +0800

    styles of up-button have been modified

---
+++ src/components/Styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+  
+export const Heading = styled.h1`
+   text-align: center;
+   color: orange;
+`;
+  
+export const Content = styled.div`
+   overflow-Y: scroll;
+   height: 2500px;
+`;
+  
+export const Button = styled.div`
+   padding-right: 1rem;
+   padding-bottom: 4rem;
+   position: fixed; 
+   width: 80%;
+   left: 50%;
+   bottom: 40px;
+   height: 20px;
+   font-size: 3rem;
+   z-index: 1;
+   cursor: pointer;
+   color: black;
+`
