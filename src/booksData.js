759880bb3bd293f427daa200668f724bb2d8c9e0
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-21 11:16:03 +0800

    booksData has been created

---
+++ src/booksData.js
@@ -0,0 +1,52 @@
+export const booksData = [
+    {
+        id: 1,
+        img: "https://miro.medium.com/max/382/0*6U4OdLVG1e25JXH8",
+        title: "Machine Learning",
+        author: "Andrew Ng",
+        source: "https://z-lib.org/",
+        topics: "Machine Learning, Deep Learning, Programming",
+        category: "Machine Learning",
+        info: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque antesollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempusviverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Doneclacinia congue felis in faucibus."
+    },
+    {
+        id: 2,
+        img: "https://images.springer.com/sgw/books/medium/9783319949888.jpg",
+        title: "Machine Learning",
+        author: "Andrew Ng",
+        source: "https://z-lib.org/",
+        topics: "Machine Learning, Deep Learning, Programming",
+        category: "Machine Learning",
+        info: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque antesollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempusviverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Doneclacinia congue felis in faucibus."
+    },
+    {
+        id: 3,
+        img: "https://images.springer.com/sgw/books/medium/9783319949888.jpg",
+        title: "Machine Learning",
+        author: "Andrew Ng",
+        source: "zlibrary.com",
+        topics: "Machine Learning, Deep Learning, Programming",
+        category: "Machine Learning",
+        info: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque antesollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempusviverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Doneclacinia congue felis in faucibus."
+    },
+    {
+        id: 4,
+        img: "https://images.springer.com/sgw/books/medium/9783319949888.jpg",
+        title: "Machine Learning",
+        author: "Andrew Ng",
+        source: "zlibrary.com",
+        category: "Machine Learning",
+        info: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque antesollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempusviverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Doneclacinia congue felis in faucibus."
+    },
+    {
+        id: 5,
+        img: "https://images.springer.com/sgw/books/medium/9783319949888.jpg",
+        title: "Machine Learning",
+        author: "Andrew Ng",
+        source: "zlibrary.com",
+        category: "Machine Learning",
+        info: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque antesollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempusviverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Doneclacinia congue felis in faucibus."
+    }
+
+   
+];
