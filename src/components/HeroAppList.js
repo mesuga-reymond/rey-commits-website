91f3d8c93efc91b28a1a8cd68e773b70605f3496
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-22 13:18:15 +0800

    Hero for applist has been added

---
+++ src/components/HeroAppList.js
@@ -0,0 +1,41 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import {Jumbotron, Container, Button, Col} from "react-bootstrap"
+
+function HeroAppList() {
+    function openTabGithub() {
+        window.open('https://github.com/Rey-commits');
+    }
+    function openTabLinkedIn() {
+        window.open('https://www.linkedin.com/in/reymondmesuga-421/'); 
+    }
+    function openTabFacebook() {
+        window.open('https://www.facebook.com/reymond.mesugah/');
+    }
+    return (
+        <div>
+            <Jumbotron fluid className="hero"> 
+                <Container className="hero-container">
+                    <h1>Front-End Web Application Projects</h1>
+                    <h5>by Reymond R. Mesuga</h5>
+                    <p>
+                        These are the collections of projects in web development that i have done so far.
+                    </p>
+                    <Col className="hero-buttons-column">
+                        <a onClick={openTabGithub}>
+                            <Button className="individual-button" variant="outline-light"><i class="fab fa-github-square social-icon"></i> GitHub</Button>
+                        </a>
+                        <a onClick={openTabLinkedIn}>
+                            <Button className="individual-button" variant="outline-light"><i class="fab fa-linkedin social-icon"></i> LinkedIn</Button>
+                        </a>
+                        <a onClick={openTabFacebook}>
+                            <Button className="individual-button" variant="outline-light"><i class="fab fa-facebook-square social-icon"></i> Facebook</Button>
+                        </a>
+                    </Col>
+                </Container>
+            </Jumbotron>
+        </div>
+    )
+}
+
+export default HeroAppList
