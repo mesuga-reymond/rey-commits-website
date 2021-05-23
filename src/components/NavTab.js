a401ddb42a45f10d5cf3d1959023a8a063dbed01
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-23 23:29:21 +0800

    new tabs has been created

--- src/components/NavTab.js
+++ src/components/NavTab.js
@@ -5,21 +5,26 @@ import {LinkContainer} from 'react-router-bootstrap'
 function NavTab() {
     return (
         <div className="navtab-container">
-            <Container>
+            <Container fluid>
             <Nav variant="tabs" defaultActiveKey="/">
                 <Nav.Item>
                     <LinkContainer to="/">
-                        <Nav.Link eventKey="link-1"><span className="nav-tab-link">Projects</span></Nav.Link>
+                        <Nav.Link eventKey="link-1"><span className="nav-tab-link">DA & AI Projects</span></Nav.Link>
+                    </LinkContainer>
+                </Nav.Item>
+                <Nav.Item>
+                    <LinkContainer to="/app-projects">
+                        <Nav.Link eventKey="link-2"><span className="nav-tab-link">Web Apps</span></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
                 <Nav.Item>
                     <LinkContainer to="/books">
-                        <Nav.Link eventKey="link-3"><span className="nav-tab-link">Book Recommendations</span></Nav.Link>
+                        <Nav.Link eventKey="link-3"><span className="nav-tab-link">Books & Notes Library</span></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
                 <Nav.Item>
                     <LinkContainer to="/about">
-                        <Nav.Link eventKey="link-2"><span className="nav-tab-link">About</span></Nav.Link>
+                        <Nav.Link eventKey="link-4"><span className="nav-tab-link">About</span></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
             </Nav>
