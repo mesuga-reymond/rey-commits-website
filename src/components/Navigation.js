e0828d97c05c5bac0565cd72135d043174327b55
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-23 23:28:46 +0800

    navigation has been modified

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,22 +4,24 @@ import {LinkContainer} from 'react-router-bootstrap'
 
 function Navigation() {
     return (
-        <div>
+        
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
-                <Navbar.Brand className="navbar-brand" href="#home">Rey-commits</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <LinkContainer to="/">
+                    <Navbar.Brand className="navbar-brand">Rey-commits<span className="navbar-copyright"><i class="fas fa-trademark"></i></span></Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle className="navbar-toggle" aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="navlinks">
                         <LinkContainer to="/">
-                            <Nav.Link>Projects</Nav.Link>
+                            <Nav.Link className="navlink-item">Projects</Nav.Link>
                         </LinkContainer>
                         <LinkContainer to="/about">
-                            <Nav.Link>About</Nav.Link>
+                            <Nav.Link className="navlink-item">About</Nav.Link> 
                         </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-        </div>
+       
     )
 }
 
