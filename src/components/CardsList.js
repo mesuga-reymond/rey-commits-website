5720bc9d58548e3749120622f265735f012be485
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-25 21:33:45 +0800

    CardList has been modified

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -1,36 +1,28 @@
 import React from 'react'
-import {Breadcrumb, Container, Row} from "react-bootstrap"
-import {LinkContainer} from 'react-router-bootstrap'
-
+import {Container, Row} from "react-bootstrap"
+// import {LinkContainer} from 'react-router-bootstrap'
 import IndividualCard from './IndividualCard'
-import {studyData} from "../studyData"
+import {studyData} from "../studyData" 
 
 function CardsList() {
-    
+
     return (
         <Container className="cardlist-container">
-            <Container className="breadcrumb-container">
-                <Breadcrumb>
-
-                    <LinkContainer to="/">
-                        <Breadcrumb.Item active>
-                            Projects
-                        </Breadcrumb.Item>
-                    </LinkContainer>
-                </Breadcrumb>
-            </Container>
-            <Row className="cardlist-row" md={2} sm={1} lg={3} >
-            {studyData.map(study => (<IndividualCard 
-            key={study.id} 
-            title={study.title} 
-            author={study.author} 
-            info={study.info} 
-            img={study.img} 
-            type={study.type}
-            modalImage={study.modalImage}
-            topics={study.topics}   
-            />))}
-            </Row>            
+            <Row className="cardlist-row" md={2} sm={1} xl={3}> 
+                {studyData.map(study => (
+                    <div id={study.id}>
+                        <IndividualCard
+                            key={study.id}
+                            title={study.title}
+                            author={study.author}
+                            info={study.info}
+                            img={study.img}
+                            type={study.type}
+                            modalImage={study.modalImage}
+                            topics={study.topics}/>
+                    </div>
+                ))}
+            </Row>
         </Container>
     )
 }
