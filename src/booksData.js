f993aaf69c6885e76cc84a60713e4288701e4438
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-26 18:15:49 +0800

    booksData content has been modified

--- src/booksData.js
+++ src/booksData.js
@@ -1,19 +1,19 @@
 export const booksData = [
     {
         id: 1,
-        img: "https://miro.medium.com/max/382/0*6U4OdLVG1e25JXH8",
-        title: "Machine Learning",
+        img: "https://imgv2-1-f.scribdassets.com/img/document/399761348/original/75a5c87340/1616077294?v=1",
+        title: "Andrew Ng's Notes",
         author: "Andrew Ng",
-        source: "https://z-lib.org/",
+        source: "https://www.kaggle.com/getting-started/102365",
         topics: "Machine Learning, Deep Learning, Programming",
         category: "Machine Learning",
-        info: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque antesollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempusviverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Doneclacinia congue felis in faucibus."
+        info: "These are the collections of notes from Andrew Ng gathered throughout his online courses in machine and deep learning."
     },
     {
         id: 2,
-        img: "https://images.springer.com/sgw/books/medium/9783319949888.jpg",
-        title: "Machine Learning",
-        author: "Andrew Ng",
+        img: "https://mitpress.mit.edu/sites/default/files/styles/large_book_cover/http/mitp-content-server.mit.edu%3A18180/books/covers/cover/%3Fcollid%3Dbooks_covers_0%26isbn%3D9780262035613%26type%3D.jpg?itok=opJUfdCT",
+        title: "Deep Learning",
+        author: " Ian Goodfellow, Yoshua Bengio, Aaron Courville",
         source: "https://z-lib.org/",
         topics: "Machine Learning, Deep Learning, Programming",
         category: "Machine Learning",
@@ -21,9 +21,9 @@ export const booksData = [
     },
     {
         id: 3,
-        img: "https://images.springer.com/sgw/books/medium/9783319949888.jpg",
-        title: "Machine Learning",
-        author: "Andrew Ng",
+        img: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1518249961l/12053731._SX318_.jpg",
+        title: "Games People Play",
+        author: "Eric Berne",
         source: "zlibrary.com",
         topics: "Machine Learning, Deep Learning, Programming",
         category: "Machine Learning",
