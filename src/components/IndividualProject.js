70e36ac847d3ec0a0d285a47a9b37480d717c14e
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-27 16:22:16 +0800

    individual button has been added

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -1,87 +1,42 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import {
-    Card, Container
-   
-} from 'react-bootstrap'
+import {Button, Card, Container} from 'react-bootstrap'
 
-function IndividualVideo({title, vlogger, thumbnail, info}) {
+function IndividualVideo({title, vlogger, thumbnail, info, source, code}) {
+    function openTabSource() {
+        window.open(source);
+    }
+    function openTabCode() {
+        window.open(code);
+    }
     return (
         <Container fluid className="individual-app-container">
             <div className="individual-app-div">
-                <Card className="individual-app" md={3} style={{
+                <Card
+                    className="individual-app"
+                    md={3}
+                    style={{
                     width: '100%'
                 }}>
                     <Card.Img variant="top" src={thumbnail}/>
                     <Card.Body>
-                        <Card.Title>{title}</Card.Title> 
+                        <Card.Title>{title}</Card.Title>
                         <p>
                             {/* <i>by {vlogger}</i> */}
                         </p>
                         <Card.Text>
                             {info}
                         </Card.Text>
-                        {/* <Button onClick={() => setLgShow(true)} variant="dark">Read more</Button> */}
-                        {/* <Modal
-                            size="lg"
-                            show={lgShow}
-                            onHide={() => setLgShow(false)}
-                            aria-labelledby="example-modal-sizes-title-lg">
-                            <Modal.Header closeButton>
-                                <Modal.Title id="example-modal-sizes-title-lg">
-                                    {type}
-                                </Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                                <Jumbotron fluid>
-                                    <Container
-                                        style={{
-                                        backgroundImage: `url(${modalImage})`
-                                    }}>
-                                        <div className="modal-jumbotron">
-                                            <h2>{title}</h2>
-                                            <div>
-                                                <i>by {author}</i>
-                                            </div>
-                                        </div>
-                                        <hr></hr>
-                                    </Container>
-                                </Jumbotron>
-                                <p>{info}</p>
-                            </Modal.Body>
-                            <div className="modal-footer">Topics: {topics}</div>
-                        </Modal> */}
+                        <a className="app-individual-button" onClick={openTabSource}>
+                            <Button variant="secondary">Link</Button>
+                        </a>
+                        <a className="app-individual-button" onClick={openTabCode}>
+                            <Button variant="secondary">Code</Button>
+                        </a>
                     </Card.Body>
                 </Card>
             </div>
-            {/* <Card style={{
-                width: '100%'
-            }}>
-                <Card.Img variant="top" src={thumbnail}/>
-                <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text>
-                        {info}
-                    </Card.Text>
-                    <Button className="individual-app-button" variant="secondary">GitHub</Button>
-                    <Button className="individual-app-button" variant="secondary">Info</Button>
-                </Card.Body>
-            </Card> */}
-            {/* <Card className="individual-video">
-                <Card.Img className="video-thumbnail" variant="top" src={thumbnail}/>
-                <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text>
-                        {info}
-                    </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem>by {vlogger}</ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-                    <Card.Link href="#">Watch</Card.Link>
-                    <Card.Link href="#">Info</Card.Link>
-                </Card.Body>
-            </Card> */}
+
         </Container>
     )
 }
