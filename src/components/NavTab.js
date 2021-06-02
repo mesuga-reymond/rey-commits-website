59d3c61c4ea35f3fbfd8682d42d6fc0fdd43ca4c
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-06-02 23:26:16 +0800

    Home tab  has been added

--- src/components/NavTab.js
+++ src/components/NavTab.js
@@ -9,22 +9,27 @@ function NavTab() {
             <Nav className="justify-content-center" variant="tabs" defaultActiveKey="/">
                 <Nav.Item>
                     <LinkContainer to="/">
-                        <Nav.Link eventKey="link-1"><span className="nav-tab-link">DA & ML Projects</span></Nav.Link>
+                        <Nav.Link eventKey="link-1"><span className="nav-tab-link">Home</span></Nav.Link>
+                    </LinkContainer>
+                </Nav.Item>
+                <Nav.Item>
+                    <LinkContainer to="/AI">
+                        <Nav.Link eventKey="link-2"><span className="nav-tab-link">DA & ML Projects</span></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
                 <Nav.Item>
                     <LinkContainer to="/app-projects">
-                        <Nav.Link eventKey="link-2"><span className="nav-tab-link">Web Apps</span></Nav.Link>
+                        <Nav.Link eventKey="link-3"><span className="nav-tab-link">Web Apps</span></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
                 <Nav.Item>
                     <LinkContainer to="/books">
-                        <Nav.Link eventKey="link-3"><span className="nav-tab-link">Books & Notes Library</span></Nav.Link> 
+                        <Nav.Link eventKey="link-4"><span className="nav-tab-link">Books & Notes Library</span></Nav.Link> 
                     </LinkContainer>
                 </Nav.Item>
                 <Nav.Item>
                     <LinkContainer to="/about">
-                        <Nav.Link eventKey="link-4"><span className="nav-tab-link">About</span></Nav.Link>
+                        <Nav.Link eventKey="link-5"><span className="nav-tab-link">About</span></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
             </Nav>
