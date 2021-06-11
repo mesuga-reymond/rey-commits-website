b010e08a0e89999ab0e8753266a1feb64c3e7527
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-06-12 07:06:21 +0800

    RandomQuote has been removed

--- src/MainScreens/MainBody.js
+++ src/MainScreens/MainBody.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import CardsList from '../components/CardsList';
 import ContentTable from '../components/ContentTable';
-import RandomQuotes from '../components/RandomQuotes';
+// import RandomQuotes from '../components/RandomQuotes';
 
 
 function MainBody() {
     return (
         <div>
-            <RandomQuotes/>
             <ContentTable/>
             <CardsList/>
         </div>
