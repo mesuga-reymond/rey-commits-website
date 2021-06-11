91465928509959acf787c5165adad17bbba392a7
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-06-11 15:07:37 +0800

    AppProjet list have been modified

--- src/components/AppProjectList.js
+++ src/components/AppProjectList.js
@@ -7,29 +7,28 @@ import RandomQuotes from './RandomQuotes'
 function AppProjectList() {
     return (
         <div>
-        <RandomQuotes />
+            <RandomQuotes/>
             <Container>
-                <AppContentTable />
+                <AppContentTable/>
                 <div fluid className="app-list-container">
-                <div className="app-list-div">
-                    <Row className="applist-row" md={2} sm={1} xl={3}>
-                        {webAppData.map(app => (
-                            <div id={app.id} >
-                                <IndividualProject
-                                    key={app.id}
-                                    title={app.title}
-                                    thumbnail={app.thumbnail}
-                                    vlogger={app.vlogger}
-                                    topics={app.topics}
-                                    info={app.info}
-                                    source={app.source}
-                                    code={app.code}
-                                    />
-                            </div>
-                        ))}
-                    </Row>
+                    <div className="app-list-div">
+                        <Row className="applist-row" md={2} sm={1} xl={3}>
+                            {webAppData.map(app => (
+                                <div id={app.id}>
+                                    <IndividualProject
+                                        key={app.id}
+                                        title={app.title}
+                                        thumbnail={app.thumbnail}
+                                        vlogger={app.vlogger}
+                                        topics={app.topics}
+                                        info={app.info}
+                                        source={app.source}
+                                        code={app.code}/>
+                                </div>
+                            ))}
+                        </Row>
+                    </div>
                 </div>
-            </div>
             </Container>
         </div>
 
