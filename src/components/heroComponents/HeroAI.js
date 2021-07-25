642e746a12713573b5ee6166b1c997d8a9ca69f4
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-07-25 08:09:25 +0800

    transfered hero components

---
+++ src/components/heroComponents/HeroAI.js
@@ -0,0 +1,40 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import {Jumbotron, Button, Col} from "react-bootstrap"
+function HeroAI() {
+    function openTabGithub() {
+        window.open('https://github.com/Rey-commits');
+    }
+    function openTabLinkedIn() {
+        window.open('https://www.linkedin.com/in/reymondmesuga-421/'); 
+    }
+    function openTabFacebook() {
+        window.open('https://www.facebook.com/reymond.mesugah/');
+    }
+    return (
+        <div>
+            <Jumbotron fluid className="hero"> 
+                <div className="hero-container-AI">
+                    <h1>Data Analysis & Machine Learning Projects</h1>
+                    <h5>by Reymond Mesuga</h5>
+                    <p>
+                        These are the collections of projects that i have been doing on my spare time and some research contributions within academics.
+                    </p>
+                    <Col className="hero-buttons-column">
+                        <a onClick={openTabGithub}>
+                            <Button className="individual-button" variant="outline-light"><i class="fab fa-github-square social-icon"></i> GitHub</Button>
+                        </a>
+                        <a onClick={openTabLinkedIn}>
+                            <Button className="individual-button" variant="outline-light"><i class="fab fa-linkedin social-icon"></i> LinkedIn</Button>
+                        </a>
+                        <a onClick={openTabFacebook}>
+                            <Button className="individual-button" variant="outline-light"><i class="fab fa-facebook-square social-icon"></i> Facebook</Button>
+                        </a>
+                    </Col>
+                </div>
+            </Jumbotron>
+        </div>
+    )
+}
+
+export default HeroAI
---
+++ src/components/heroComponents/HeroHome.js
@@ -0,0 +1,42 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import { Button, Col, Jumbotron } from 'react-bootstrap';
+
+function HeroHome() {
+    function openTabGithub() {
+        window.open('https://github.com/Rey-commits');
+    }
+    function openTabLinkedIn() {
+        window.open('https://www.linkedin.com/in/reymondmesuga-421/'); 
+    }
+    function openTabFacebook() {
+        window.open('https://www.facebook.com/reymond.mesugah/');
+    }
+    return (
+        <div fluid>
+            <Jumbotron fluid className="hero"> 
+                <div className="hero-container-AI">
+                <div>Welcome to</div>
+                    <h1 className="home-hero-title home-hero">Rey-commits</h1>
+                    <p>This is Reymond's home for creativity and innovation</p>
+                    {/* <p>
+                        These are the collections of projects that i have been doing on my spare time and some research contributions within academics.
+                    </p> */}
+                    <Col className="hero-buttons-column">
+                        <a onClick={openTabGithub}>
+                            <Button className="individual-button" variant="outline-light"><i class="fab fa-github-square social-icon"></i> GitHub</Button>
+                        </a>
+                        <a onClick={openTabLinkedIn}>
+                            <Button className="individual-button" variant="outline-light"><i class="fab fa-linkedin social-icon"></i> LinkedIn</Button>
+                        </a>
+                        <a onClick={openTabFacebook}>
+                            <Button className="individual-button" variant="outline-light"><i class="fab fa-facebook-square social-icon"></i> Facebook</Button>
+                        </a>
+                    </Col>
+                </div>
+            </Jumbotron>
+        </div>
+    )
+}
+
+export default HeroHome
