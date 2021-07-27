7b9487af559ef7f2bb4444d43bcfd1c32c8ee7f8
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-07-28 06:29:47 +0800

    large cards have been separated

--- src/components/bodyComponents/CardsList.js
+++ src/components/bodyComponents/CardsList.js
@@ -3,6 +3,7 @@ import {Container, Row} from "react-bootstrap"
 // import {LinkContainer} from 'react-router-bootstrap'
 import IndividualCard from '../indivudualComponents/IndividualCard'
 import {studyData} from "../../Data/studyData"
+import {studyDataForLarge} from "../../Data/studyData"
 import ContentTable from '../ContentTable'
 
 function CardsList() {
@@ -13,6 +14,21 @@ function CardsList() {
             <Container className="cardlist-container">
                 <Row className="cardlist-row" md={2} sm={1} xl={3}>
                     {studyData.map(study => (
+                        <div id={study.id}>
+                            <IndividualCard
+                                key={study.id}
+                                title={study.title}
+                                author={study.author}
+                                info={study.info}
+                                img={study.img}
+                                type={study.type}
+                                modalImage={study.modalImage}
+                                code={study.code}/>
+                        </div>
+                    ))}
+                </Row>
+                <Row className="cardlist-row" md={1} sm={1} xl={2}>
+                    {studyDataForLarge.map(study => (
                         <div id={study.id}>
                             <IndividualCard
                                 key={study.id}
