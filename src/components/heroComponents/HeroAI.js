df4a5bd4fe6086fbfbdc96b53bb16c54ebc83554
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-08-21 19:41:48 +0800

    buttons have been modified

--- src/components/heroComponents/HeroAI.js
+++ src/components/heroComponents/HeroAI.js
@@ -14,7 +14,7 @@ function HeroAI() {
     return (
         <div>
             <Jumbotron fluid className="hero-AI"> 
-                <div className="hero-container-AI">
+                <div className="hero-container">
                     <h1 className="hero-text">Artificial Intelligence</h1>
                     <h1 className="hero-text">Projects</h1>
                     <h5 className="hero-text">by Reymond Mesuga</h5>
@@ -22,16 +22,13 @@ function HeroAI() {
                     <Col className="hero-buttons-column">
                     <a onClick={openTabGithub}>
                             <Button className="individual-button" variant="dark"><i class="fab fa-github-square social-icon"></i> GitHub</Button>
-                            <Button className="individual-button-small-device" variant="dark"><i class="fab fa-github-square social-icon"></i> GitHub</Button>
                             
                         </a>
                         <a onClick={openTabLinkedIn}>
                             <Button className="individual-button" variant="dark"><i class="fab fa-linkedin social-icon"></i> LinkedIn</Button>
-                            <Button className="individual-button-small-device" variant="dark"><i class="fab fa-linkedin social-icon"></i> LinkedIn</Button>
                         </a>
                         <a onClick={openTabFacebook}>
                             <Button className="individual-button" variant="dark"><i class="fab fa-facebook-square social-icon"></i> Facebook</Button>
-                            <Button className="individual-button-small-device" variant="dark"><i class="fab fa-facebook-square social-icon"></i> Facebook</Button>
                         </a>
                     </Col>
                 </div>
