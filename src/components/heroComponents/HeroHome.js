86c080150ed2d1033d097d0135126a24a5736794
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-08-21 19:52:11 +0800

    buttons have been modified

--- src/components/heroComponents/HeroHome.js
+++ src/components/heroComponents/HeroHome.js
@@ -16,7 +16,7 @@ function HeroHome() {
     return (
         <div fluid>
             <Jumbotron fluid className="hero-home"> 
-                <div className="hero-container-AI">
+                <div className="hero-container">
                 <div className="hero-text">Welcome to</div>
                     <h1 className="home-hero-title home-hero hero-text">Rey-commits</h1>
                     <p className="hero-text">This is Reymond's home for creativity and innovation</p>
@@ -27,16 +27,13 @@ function HeroHome() {
                     <Col className="hero-buttons-column">
                         <a onClick={openTabGithub}>
                             <Button className="individual-button" variant="dark"><i class="fab fa-github-square social-icon"></i> GitHub</Button>
-                            <Button className="individual-button-small-device" variant="dark"><i class="fab fa-github-square social-icon"></i> GitHub</Button>
                             
                         </a>
                         <a onClick={openTabLinkedIn}>
                             <Button className="individual-button" variant="dark"><i class="fab fa-linkedin social-icon"></i> LinkedIn</Button>
-                            <Button className="individual-button-small-device" variant="dark"><i class="fab fa-linkedin social-icon"></i> LinkedIn</Button>
                         </a>
                         <a onClick={openTabFacebook}>
                             <Button className="individual-button" variant="dark"><i class="fab fa-facebook-square social-icon"></i> Facebook</Button>
-                            <Button className="individual-button-small-device" variant="dark"><i class="fab fa-facebook-square social-icon"></i> Facebook</Button>
                         </a>
                     </Col>
                 </div>
