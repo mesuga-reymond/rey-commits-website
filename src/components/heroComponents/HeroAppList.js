3d771008aa221452154f0d187ce9709ceddcd91c
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-08-21 19:46:23 +0800

    buttons have been modified

--- src/components/heroComponents/HeroAppList.js
+++ src/components/heroComponents/HeroAppList.js
@@ -16,24 +16,20 @@ function HeroAppList() {
     return (
         <div>
             <Jumbotron fluid className="hero-app"> 
-                <div className="hero-container-AI">
+                <div className="hero-container">
                     <h1 className="hero-text">Web App Development</h1>
                     <h1 className="hero-text">Projects</h1>
                     <h5 className="hero-text">by Reymond Mesuga</h5>
                    
                     <Col className="hero-buttons-column">
                     <a onClick={openTabGithub}>
-                            <Button className="individual-button" variant="dark"><i class="fab fa-github-square social-icon"></i> GitHub</Button>
-                            <Button className="individual-button-small-device" variant="dark"><i class="fab fa-github-square social-icon"></i> GitHub</Button>
-                            
+                            <Button className="individual-button" variant="dark"><i class="fab fa-github-square social-icon"></i> GitHub</Button>  
                         </a>
                         <a onClick={openTabLinkedIn}>
                             <Button className="individual-button" variant="dark"><i class="fab fa-linkedin social-icon"></i> LinkedIn</Button>
-                            <Button className="individual-button-small-device" variant="dark"><i class="fab fa-linkedin social-icon"></i> LinkedIn</Button>
                         </a>
                         <a onClick={openTabFacebook}>
                             <Button className="individual-button" variant="dark"><i class="fab fa-facebook-square social-icon"></i> Facebook</Button>
-                            <Button className="individual-button-small-device" variant="dark"><i class="fab fa-facebook-square social-icon"></i> Facebook</Button>
                         </a>
                     </Col>
                 </div>
