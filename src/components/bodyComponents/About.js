deddf9ef4d8d06bde24016bdbc72317636e1ac89
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-08-23 21:33:54 +0800

    content has been modified

--- src/components/bodyComponents/About.js
+++ src/components/bodyComponents/About.js
@@ -5,37 +5,41 @@ import {Button, Card, Container, Image} from 'react-bootstrap'
 
 function About() {
     function openTabResume() {
-        window.open("https://github.com/Rey-commits/rey-commits-website/raw/master/src/components/bodyComponents/Mesuga_Reymond_Resume.pdf");
+        window.open("https://github.com/Rey-commits/rey-commits-website/raw/master/src/components/bod" +
+                "yComponents/Mesuga_Reymond_Resume.pdf");
     }
     return (
         <div>
-                    <Container className="about-container"> 
-                        <h1>About Me</h1>
-                        <div className="about-me-line">
-                            <hr></hr>
-                        </div>
-                        <Image className="profile-image" src="reymond2.jpg" roundedCircle/>
-                        <h3>Reymond R. Mesuga</h3>
-                        <a className="app-individual-button" onClick={openTabResume}>
-                            <Button variant="secondary">Download Resume</Button>
-                        </a>
-                        
-                        <Card.Text className="about-info">
-                                        <p>React App Development and Artificial Intelligence Enthusiast</p>
-                                        <p><i>
-                                        I'm a programmer and a physics student at PUP Manila. I enjoy doing data
-                                        analysis, artificial intelligence and app development on my spare time. 
-                                        I made this website mainly to showcase the projects
-                                        that i have done so far including personal projects and academic research
-                                        contributions that is related to data analysis and artificial intelligence 
-                                        where i use Python librarires. When it comes to app development, i use javascript 
-                                        as my main languange and especially its framework called React JS. 
-                                        In fact, i made this website using React JS and work on it for almost 
-                                        three days before it looks decent and still working on the improvement at the moment. 
-                                        For more info, follow me on GitHub or connect with me via LinkedIn.
-                                        </i></p>
-                        </Card.Text>
-                </Container>
+            <Container className="about-container">
+                <h1>About Me</h1>
+                <div className="about-me-line">
+                    <hr></hr>
+                </div>
+                <Image className="profile-image" src="reymond2.jpg" roundedCircle/>
+                <h3>Reymond R. Mesuga</h3>
+                <a className="app-individual-button" onClick={openTabResume}>
+                    <Button variant="secondary">Download Resume</Button>
+                </a>
+
+                <Card.Text className="about-info">
+                    <p>React App Development and Artificial Intelligence Enthusiast</p>
+                    <p>
+                        <i>
+                            I'm a programmer and a physics student at PUP Manila. I enjoy doing data
+                            analysis, artificial intelligence and app development on my spare time. I made
+                            this website mainly to showcase the projects that i have done so far including
+                            personal projects and academic research contributions that is related to data
+                            analysis and artificial intelligence where i use Python librarires. When it
+                            comes to app development, i use javascript as my main languange and especially
+                            its framework called React JS. In fact, i made this website using React JS and
+                            work on it for almost three days before it looks decent and still working on the
+                            improvement at the moment. For more info, follow me on GitHub or connect with me
+                            via LinkedIn.
+                        </i>
+                    </p>
+                </Card.Text>
+            
+            </Container>
         </div>
     )
 }
