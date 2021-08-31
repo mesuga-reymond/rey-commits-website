8474a206cf0387038a0f1b4434533a3b5f6d40c3
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-09-01 00:10:45 +0800

    lines have been added

--- src/components/bodyComponents/Home.js
+++ src/components/bodyComponents/Home.js
@@ -16,12 +16,18 @@ function Home() {
             <Container fluid>
                 <div fluid className="home-container">
                     <h3 className="home-upper-text">Topics & Services</h3>
+                    <div className="line">
+                        <hr></hr>
+                    </div>
                     <div className="topic-collection">
                         <Row fluid className="individual-row">
                             <Col fluid lg={4} md={4} sm={12} className="home-individual-info">
                                 <Figure.Image className="home-image" alt="171x180" src={ML}/>
                                 <Figure.Image className="home-image-black" alt="171x180" src={ML_black}/>
                                 <h3 className="home-info-title">Data Analysis</h3>
+                                <div className="line">
+                                    <hr></hr>
+                                </div>
                                 <p className="info-text">
                                 Data analysis is a process of inspecting, cleansing, transforming, and modeling 
                                 data with the goal of discovering useful information, informing conclusions, and 
@@ -34,6 +40,9 @@ function Home() {
                                 <Figure.Image className="home-image" alt="171x180" src={AIimage}/>
                                 <Figure.Image className="home-image-black" alt="171x180" src={AIimage_black}/>
                                 <h3 className="home-info-title">Artificial Intelligence</h3>
+                                <div className="line">
+                                    <hr></hr>
+                                </div>
                                 <p className="info-text">
                                 Artificial intelligence (AI) refers to the simulation of human intelligence 
                                 in machines that are programmed to think like humans and mimic their actions. 
@@ -47,6 +56,9 @@ function Home() {
                                 <Figure.Image className="home-image" alt="171x180" src={webdev}/>
                                 <Figure.Image className="home-image-black" alt="171x180" src={webdev_black}/>
                                 <h3 className="home-info-title">Web App Development</h3>
+                                <div className="line">
+                                    <hr></hr>
+                                </div>
                                 <p className="info-text">
                                 Web development is the building and maintenance of websites; it’s the work that 
                                 happens behind the scenes to make a website look great, work fast and perform well 
