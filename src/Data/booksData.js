34ab28f0944a5f8a05d6d43a186497e4a7d5c819
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-09-05 22:39:38 +0800

    content has been modified

--- src/Data/booksData.js
+++ src/Data/booksData.js
@@ -11,16 +11,6 @@ export const booksData = [
     },
     {
         id: 2,
-        img: "https://mitpress.mit.edu/sites/default/files/styles/large_book_cover/http/mitp-content-server.mit.edu%3A18180/books/covers/cover/%3Fcollid%3Dbooks_covers_0%26isbn%3D9780262035613%26type%3D.jpg?itok=opJUfdCT",
-        title: "Deep Learning",
-        author: " Ian Goodfellow, Yoshua Bengio, Aaron Courville",
-        source: "https://z-lib.org/",
-        topics: "Machine Learning, Deep Learning, Programming",
-        category: "Machine Learning",
-        info: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque antesollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempusviverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Doneclacinia congue felis in faucibus."
-    },
-    {
-        id: 3,
         img: "notes-images/ANN-for-beginners.png",
         title: "Artificial Neural Networks for Beginners",
         author: "Carlos Gershenson",
@@ -30,7 +20,7 @@ export const booksData = [
         info: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque antesollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempusviverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Doneclacinia congue felis in faucibus."
     },
     {
-        id: 4,
+        id: 3,
         img: "https://simg.nicepng.com/png/small/80-804192_smartlogic-explores-javascript-react-and-flux-tech.png",
         title: "The React Cheatsheet for 2021‬ (+ Real-World Examples)",
         author: "Reed Barger",
