129395d026aeb6cd88500b05875cee21b59968af
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-11-20 06:10:22 +0800

    title has been modified

--- src/components/heroComponents/HeroAI.js
+++ src/components/heroComponents/HeroAI.js
@@ -1,40 +1,20 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import {Jumbotron, Button, Col, Image} from "react-bootstrap"
+import {Jumbotron, Image} from "react-bootstrap"
+import ContactButtons from '../identity_and_contacts/contactButtons';
+import AITitle from './heroTitles/AI_Title';
 function HeroAI() {
-    function openTabGithub() {
-        window.open('https://github.com/Rey-commits');
-    }
-    function openTabLinkedIn() {
-        window.open('https://www.linkedin.com/in/reymondmesuga-421/'); 
-    }
-    function openTabFacebook() {
-        window.open('https://www.facebook.com/reymond.mesugah/');
-    }
+    
     return (
         <div>
-            <Jumbotron fluid className="hero-AI"> 
+            <Jumbotron fluid className="hero-AI">
                 <div className="hero-container">
                 <div className="profile-div">
                 <Image className="home-profile-image-large" src="reymond2.jpg" roundedCircle/>
                 <Image className="home-profile-image-medium" src="reymond2.jpg" roundedCircle/>
                 </div>
-                    <h1 className="hero-text">Artificial Intelligence</h1>
-                    <h1 className="hero-text">Projects</h1>
-                    <h5 className="hero-text">by Reymond Mesuga</h5>
-                    
-                    <Col className="hero-buttons-column">
-                    <a onClick={openTabGithub}>
-                            <Button className="individual-button" variant="dark"><i class="fab fa-github-square social-icon"></i> GitHub</Button>
-                            
-                        </a>
-                        <a onClick={openTabLinkedIn}>
-                            <Button className="individual-button" variant="dark"><i class="fab fa-linkedin social-icon"></i> LinkedIn</Button>
-                        </a>
-                        <a onClick={openTabFacebook}>
-                            <Button className="individual-button" variant="dark"><i class="fab fa-facebook-square social-icon"></i> Facebook</Button>
-                        </a>
-                    </Col>
+                    <AITitle />
+                    <ContactButtons />
                 </div>
             </Jumbotron>
         </div>
