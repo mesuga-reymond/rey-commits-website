a6a9b37791f5000100cb5b3f1e2f9bc01c37a8ff
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-11-20 09:14:58 +0800

    name and contacts haven been modified

--- src/components/identity_and_contacts/name.js
+++ src/components/identity_and_contacts/name.js
@@ -1,7 +1,7 @@
 function Name() {
     return (
         <div>
-            <h1 className="home-hero-title home-hero hero-text">Reymond Mesuga</h1>
+            <h1 className="home-hero hero-text">Reymond Mesuga</h1>
             <h5 className="hero-text">Physics | Artificial Intelligence | React Programming</h5>
         </div>
     )
