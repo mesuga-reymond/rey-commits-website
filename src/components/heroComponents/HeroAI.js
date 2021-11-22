d65086390e60010a5dc6681fd6a6a13e0fb009a4
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-11-23 06:29:02 +0800

    jet has been added

--- src/components/heroComponents/HeroAI.js
+++ src/components/heroComponents/HeroAI.js
@@ -4,17 +4,24 @@ import {Jumbotron, Image} from "react-bootstrap"
 import ContactButtons from '../identity_and_contacts/contactButtons';
 import AITitle from './heroTitles/AI_Title';
 function HeroAI() {
-    
+
     return (
         <div>
             <Jumbotron fluid className="hero-AI">
                 <div className="hero-container">
-                <div className="profile-div">
-                <Image className="home-profile-image-large" src="reymond2.jpg" roundedCircle/>
-                <Image className="home-profile-image-medium" src="reymond2.jpg" roundedCircle/>
-                </div>
-                    <AITitle />
-                    <ContactButtons />
+                    <div className="profile-div">
+                        <Image className="home-profile-image-large" src="reymond2.jpg" roundedCircle/>
+                        <Image className="home-profile-image-medium" src="reymond2.jpg" roundedCircle/>
+                    </div>
+                    <div className="large-screen-name"> 
+                    <AITitle/>
+                    </div>
+                    <div className="small-screen-name"> 
+                    <AITitle/>
+                    </div>
+
+                    
+                    <ContactButtons/>
                 </div>
             </Jumbotron>
         </div>
