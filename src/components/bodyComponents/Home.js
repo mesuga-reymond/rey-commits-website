59dd28c2ae645df0af530d79f5a93e50495f663a
Reymond Mesuga <mesugareymondrobin@gmail.com>
2022-01-04 08:38:25 +0800

    descriptions included

--- src/components/bodyComponents/Home.js
+++ src/components/bodyComponents/Home.js
@@ -64,7 +64,7 @@ function Home() {
                             skills="C, CSS,CoffeeScript, HTML, JSON, JavaScript, Jupyter Notebook, SCSS, TypeScript, Other"
                             username="mesuga-reymond"></codersrank-skills-chart>
                     </div>
-                    <h3 className="home-upper-text">Topics & Services</h3>
+                    <h3 className="home-upper-text">Domain Services</h3>
                     <div className="line">
                         <hr></hr>
                     </div>
@@ -77,13 +77,13 @@ function Home() {
                                 <div className="line">
                                     <hr></hr>
                                 </div>
-                                {/* <p className="info-text">
+                                <p className="info-text">
                                     Data analysis is a process of inspecting, cleansing, transforming, and modeling
                                     data with the goal of discovering useful information, informing conclusions, and
                                     supporting decision-making. The data analysis tools that i often use are Python
                                     modules such as numpy, pandas, matplotlib, seaborn and SQL. For scientific
                                     computations, i also use Octave, Scilab and MatLab.
-                                </p> */}
+                                </p>
                             </Col>
                             <Col fluid lg={4} md={4} sm={12} className="home-individual-info">
                                 <Figure.Image className="home-image" alt="171x180" src={AIimage}/>
@@ -92,14 +92,14 @@ function Home() {
                                 <div className="line">
                                     <hr></hr>
                                 </div>
-                                {/* <p className="info-text">
+                                <p className="info-text">
                                     Artificial intelligence (AI) refers to the simulation of human intelligence in
                                     machines that are programmed to think like humans and mimic their actions. The
                                     term may also be applied to any machine that exhibits traits associated with a
                                     human mind such as learning and problem-solving. The Artificial Intelligence
                                     involve in my projects uses machine learning and deep learning using Python
                                     libraries such as TensorFlow, PyTorch and Scikit-Learn.
-                                </p> */}
+                                </p>
                             </Col>
                             <Col fluid lg={4} md={4} sm={12} className="home-individual-info">
                                 <Figure.Image className="home-image" alt="171x180" src={webdev}/>
@@ -108,13 +108,13 @@ function Home() {
                                 <div className="line">
                                     <hr></hr>
                                 </div>
-                                {/* <p className="info-text">
+                                <p className="info-text">
                                     Web development is the building and maintenance of websites; it’s the work that
                                     happens behind the scenes to make a website look great, work fast and perform
                                     well with a seamless user experience. I mainly focus on the front-end
                                     development where i use React JS as my JavaScript framework and other CSS
                                     frameworks such as React-Bootstrap.
-                                </p> */}
+                                </p>
                             </Col>
                         </Row>
                         <TechsLogoList/>
