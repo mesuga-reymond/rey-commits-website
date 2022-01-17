932206531fa0bd5a8f267d009b98546719c52f30
Reymond Mesuga <mesugareymondrobin@gmail.com>
2022-01-17 15:15:08 +0800

    bg music modified

--- src/components/bodyComponents/BGMusic.js
+++ src/components/bodyComponents/BGMusic.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function BGMusic(){
     return(
-        <iframe className='bg-music' title="music"  width="560" height="315" src="https://www.youtuberepeater.com/watch?v=eFHdRkeEnpM#gsc.tab=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe className='bg-music' title="music"  width="560" height="315" src="https://www.youtuberepeater.com/watch?v=zBY5bVTxOAw#gsc.tab=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     )
 }
 
