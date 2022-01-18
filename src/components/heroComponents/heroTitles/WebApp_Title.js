2008870292e0eec904b7d618251b26db836039df
Reymond Mesuga <mesugareymondrobin@gmail.com>
2022-01-18 11:54:37 +0800

    title moodified

--- src/components/heroComponents/heroTitles/WebApp_Title.js
+++ src/components/heroComponents/heroTitles/WebApp_Title.js
@@ -1,10 +1,10 @@
 function WebAppTitle() {
     return (
-        <>
+        <div>
             <h1 className="hero-text">Web App Development</h1>
             <h1 className="hero-text">Projects</h1>
             <h5 className="hero-text">by Reymond Mesuga</h5>
-        </>
+        </div>
     )
 }
 export default WebAppTitle;  
