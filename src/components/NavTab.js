79e16979c226dacc57e7d039f2f19f96b1084b43
Reymond Mesuga <mesugareymondrobin@gmail.com>
2022-01-18 10:56:36 +0800

    button has been added

--- src/components/NavTab.js
+++ src/components/NavTab.js
@@ -1,35 +1,35 @@
 import React from 'react'
-import {Container, Nav} from 'react-bootstrap'
+import {Button, Container, Nav} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap' 
 
 function NavTab() {
     return (
         <div className="navtab-container">
             <Container fluid>
-            <Nav className="justify-content-center" variant="tabs" defaultActiveKey="/">
+            <Nav className="justify-content-center" defaultActiveKey="/">
                 <Nav.Item>
-                    <LinkContainer to="/">
-                        <Nav.Link eventKey="link-1"><span className="nav-tab-link">Home</span></Nav.Link>
+                    <LinkContainer to="">
+                        <Nav.Link eventKey="1"><span className="nav-tab-link"><Button variant="outline-dark">Home</Button></span></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
                 <Nav.Item>
                     <LinkContainer to="/AI">
-                        <Nav.Link eventKey="link-2"><span className="nav-tab-link">A.I. Projects</span></Nav.Link>
+                        <Nav.Link eventKey="2"><span className="nav-tab-link"><Button variant="outline-dark">A.I. Projects</Button></span></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
                 <Nav.Item>
                     <LinkContainer to="/app-projects">
-                        <Nav.Link eventKey="link-3"><span className="nav-tab-link">Web Apps</span></Nav.Link>
+                        <Nav.Link eventKey="3"><span className="nav-tab-link"><Button variant="outline-dark">Web Apps</Button></span></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
                 <Nav.Item>
                     <LinkContainer to="/books">
-                        <Nav.Link eventKey="link-4"><span className="nav-tab-link">Books & Notes Library</span></Nav.Link> 
+                        <Nav.Link eventKey="4"><span className="nav-tab-link"><Button variant="outline-dark">Books & Notes Library</Button></span></Nav.Link> 
                     </LinkContainer>
                 </Nav.Item>
                 <Nav.Item>
                     <LinkContainer to="/about">
-                        <Nav.Link eventKey="link-5"><span className="nav-tab-link">About</span></Nav.Link>
+                        <Nav.Link eventKey="5"><span className="nav-tab-link"><Button variant="outline-dark">About</Button></span></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
             </Nav>
